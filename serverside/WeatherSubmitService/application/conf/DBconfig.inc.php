9b2f299e9d0df9aefcf79fa590ed56c028e471b7
JREB <jreb1990@gmail.com>
2013-03-02 13:55:36 +0100

    Initial commit for SubmitService

---
+++ serverside/WeatherSubmitService/application/conf/DBconfig.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+$db_server = "localhost";
+$db_name = "db_name";
+$db_keytable = "qild9_weatherpi_raspi";
+$db_arduinotable = "qild9_weatherpi_arduino";
+$db_weathertable = "qild9_weatherpi_weather_data";
+$db_weathertable_continuous = "qild9_weatherpi_weather_data_continous";
+$db_weathertable_daily = "qild9_weatherpi_weather_data_daily";
+$db_user = "user";
+$db_pw = "pw";
+
+
+?>
---
+++ serverside/WeatherSubmitService/application/submitService.php
@@ -0,0 +1,215 @@
+<?php
+
+
+/*
+ * The 2 different needed json structures:
+*
+* '{"arduino-id": "some-id",
+* 	"date": "2013-02-27",
+*  "type": "continuous",
+*  "data": {
+*		"time": "13:32:54",
+*  	"temperature": 20,
+*      "rainfall": 700,
+*      "wind_direction": "NNO",
+*      "wind_speed": 65,
+*      "air_pressure": 67,
+*      "light_intensity": 120
+*  }
+* }'
+*
+* '{"arduino-id": "some-id",
+* 	"date": "2013-02-27",
+*  "type": "daily",
+*  "data": {
+*  	"sunrise": "06:43:43",
+*      "sunset": "20:23:34",
+*      "sunlight_hours": 11,
+*  }
+* }'
+*
+*/
+
+
+if($_POST["apikey"] && sizeof($_POST["apikey"]) == 20) {
+
+	include_once("conf/DBconfig.inc.php");
+	include_once("lib/phpseclib/Crypt/RSA.php");
+
+	if(validateKey($_POST["apikey"])) {
+
+		$privateKey = getPrivateKey();
+
+		$message = decryptMessage($privateKey, $_POST["msg"]);
+
+		$data = json_decode($message);
+
+		insertIntoDB($data);
+
+		returnResponse("200", "Successfully inserted");
+
+
+	} else {
+		returnResponse("403", "API-key not valid");
+	}
+
+
+
+
+} else {
+	returnResponse("401", "No API-key submitted");
+}
+
+
+// Check if API-key is in the database
+function validateKey($key) {
+
+	$valid = false;
+
+	$db = mysqli_connect($db_server, $db_user, $db_pw, $db_name);
+
+	$sql = "SELECT * FROM ".$db_keytable." WHERE api_key='?'";
+
+	$statement = $db->prepare($sql);
+	if(!$statement) {
+		die ("Query couldn't be prepared... ".$db->error);
+	}
+
+	$statement->bind_param("s", $key);
+	$result = $statement->execute();
+
+	if (!$result) {
+		die("Query couldn't be executed: ".$db->error);
+	}
+
+	if ($result->num_rows)
+		$valid = true;
+
+	mysqli_close($db);
+
+	return valid;
+}
+
+// Fetch private key from database
+function getPrivateKey() {
+
+	$privateKey = "";
+
+	return $privateKey;
+}
+
+
+// Decrypt the message
+function decryptMessage($pk, $msg) {
+
+	$rsa = new Crypt_RSA();
+	$rsa->loadKey($pk);
+	$plainMsg = $rsa->decrypt($msg);
+
+	return $plainMsg;
+}
+
+// Insert the data into the DB
+function insertIntoDB($data_object) {
+
+	$db = mysqli_connect($db_server, $db_user, $db_pw, $db_name);
+
+	$arduino_id = $data_object->{'arduino-id'};
+	$type = $data_object->{'type'};
+	$date = $data_object->{'date'};
+	$weather_data = $data_object->{'data'};
+
+	// Get the current location of the arduino
+	$sql = "SELECT location_id FROM ".$db_arduinotable." WHERE arduino_id=?";
+	$statement = $db->prepare($sql);
+	if(!$statement) {
+		die ("Query couldn't be prepared... ".$db->error);
+	}
+	$statement->bind_param("s", $arduino_id);
+	if($result->num_rows > 0) {
+		// location_id found --> arduino exists
+		$row = $result->fetch_assoc();
+		$location_id = $row["location_id"];
+
+		// Check if there already exists an entry for this id at this date and the current location
+		$sql = "SELECT weather_id FROM ".$db_weathertable." WHERE arduino_id=? AND date=? AND location_id=?";
+
+		$statement = $db->prepare($sql);
+		if(!$statement) {
+			die ("Query couldn't be prepared... ".$db->error);
+		}
+		$statement->bind_param("sss", $arduino_id, $date, $location_id);
+		$result = $statement->execute();
+		if($result->num_rows > 0) {
+			// entry already exists --> get weather_id
+			$row = $result->fetch_assoc();
+			$weather_id = $row["weather_id"];
+
+
+		} else {
+			// entry doesn't exist --> create a new one
+			$sql = "INSERT INTO ".$db_weathertable." (arduino_id, date, location_id) VALUES(?, ?, ?)";
+			$statement = $db->prepare($sql);
+			if(!$statement) {
+				die ("Query couldn't be prepared... ".$db->error);
+			}
+			$statement->bind_param("sss", $arduino_id, $date, $location_id);
+			if(!$statement->execute()) {
+				die("Query couldn't be executed: ".$db->error);
+			}
+			
+			// get the weather_id of the inserted entry
+			$sql = "SELECT weather_id FROM ".$db_weathertable." WHERE arduino_id=? AND date=? AND location_id=?";
+			
+			$statement = $db->prepare($sql);
+			if(!$statement) {
+				die ("Query couldn't be prepared... ".$db->error);
+			}
+			$statement->bind_param("sss", $arduino_id, $date, $location_id);
+			$result = $statement->execute();
+			$row = $result->fetch_assoc();
+			$weather_id = $row["weather_id"];
+		}
+		
+		// weather_id has been fetched --> now insert the actual data
+		
+		
+		
+
+	} else {
+		// arduino_id not found --> error
+	}
+
+
+
+
+
+
+
+	$sql = "INSERT INTO ".$db_weathertable." (feld1, feld2, feld3) VALUES(?, ?, ?)";
+
+	$statement = $db->prepare($sql);
+	if(!$statement) {
+		die ("Query couldn't be prepared... ".$db->error);
+	}
+
+	$statement->bind_param("sss", "", "", "");
+	if(!$statement->execute()) {
+		die("Query couldn't be executed: ".$db->error);
+	}
+
+
+
+	mysqli_close($db);
+
+}
+
+
+// Forge a custom response
+function returnResponse($statusCode, $message) {
+
+}
+
+
+
+?>
