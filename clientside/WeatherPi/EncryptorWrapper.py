e8fe7171f333831ba5260a2aad5643dd60873d6b
glyxbaer <chwolf91@gmail.com>
2013-03-04 15:04:21 +0100

    added encryption, should now work fine

--- clientside/WeatherPi/EncryptorWrapper.py
+++ clientside/WeatherPi/EncryptorWrapper.py
@@ -1,2 +1,27 @@
+import M2Crypto, os,sys,Crypto
+from M2Crypto import EVP
+from Crypto.Cipher import AES
+from Crypto import Random
+
+
 def encryptData(data, pubKeyPath):
-	return data
+	
+	rsa = M2Crypto.RSA.load_pub_key(pubKeyPath)
+	return rsa.public_encrypt(data, M2Crypto.RSA.pkcs1_oaep_padding)
+	
+
+def decryptData(data, privKeyPath):
+	rsa = M2Crypto.RSA.load_key (privKeyPath)
+	return rsa.private_decrypt(data, M2Crypto.RSA.pkcs1_oaep_padding)
+
+def generateIV():
+	return Random.new().read(AES.block_size)
+
+def encryptDataAES(plaintext, key, iv):
+	
+	
+	cipher = Crypto.Cipher.AES.new(key, Crypto.Cipher.AES.MODE_CFB, iv)
+	return cipher.encrypt(plaintext)
+
+def getSessionkey():
+	return Random.new().read(16)
--- clientside/WeatherPi/Sender.py
+++ clientside/WeatherPi/Sender.py
@@ -1,6 +1,6 @@
 import ConfigParser, os, logging
-from EncryptorWrapper import encryptData
-import urllib, urllib2
+from EncryptorWrapper import encryptData, decryptData, getSessionkey,encryptDataAES
+import urllib, urllib2, sys, json
 
 class RaspiSubmitter():
 	def getData(self):
@@ -8,23 +8,31 @@ class RaspiSubmitter():
 
 	def submit(self):
 		config = Configer('config/config.cfg') 
-		for jsonfile in getFiles():
-			jsonfile = "data/todo/" + jsonfile
-			filecontent = file_get_contents(jsonfile)
-			data = "apikey=" + config.apikey + "data = " + encryptData(filecontent, "conf/RSA.pub")
-			req = urllib2.Request(config.server, data)
+		for weatherfile in getFiles():
 			
-
+			# Create sessionkey and initialization value for AES decryption
+			sessionkey = getSessionkey()
+			iv= os.urandom(16)
+			
+			# Retrieve a dictionary from the file which contains the according data in json format and add a field which contains the date. 
+			# This date is saved in the filename
+			json_data = retrieveJsonAsTextAndAddTime("data/todo/" + weatherfile)
+			
+			#Dump the dict as a string in json format
+			filecontent = json.dumps(json_data)
+			
+			#prepare the payload for the POST request and send it
+			data={'apikey':config.apikey,'iv': iv ,'sessionkey': encryptData(sessionkey, config.pubKeyPath), 'msg':encryptDataAES(filecontent, sessionkey, iv)}
+			req = urllib2.Request(config.server, urllib.urlencode(data))
+			
 			try:
 				response = urllib2.urlopen(req)
 				if response.getcode == 401:
 					pass #todo: copyfile
+
 			except urllib2.URLError, e:
-				logging.error("Could not post data from file '%s' (%s)", jsonfile, str(e))
-
-
-
-
+				logging.error("Could not post data from file '%s' (%s)", weatherfile, str(e))
+				exit(1)
 
 class Configer():
 	def __init__(self, filename):
@@ -33,6 +41,7 @@ class Configer():
 		self.config.read(self.filename)
 		self.server = self.config.get('main', 'server')
 		self.apikey = self.config.get('main', 'apikey')
+		self.pubKeyPath = self.config.get('main', 'pubKeyPath')
 
 def getFiles():
 	for f in os.listdir("data/todo"):
@@ -42,3 +51,12 @@ def getFiles():
 def file_get_contents(filename):
 	with open(filename) as f:
 		return f.read()
+
+def retrieveJsonAsTextAndAddTime(jsonFileName):
+		js = open(jsonFileName)
+		json_data = json.load(js)
+		js.close()
+
+		json_data['date']= jsonFileName.split('/')[-1].split('.')[0]
+		
+		return json_data
